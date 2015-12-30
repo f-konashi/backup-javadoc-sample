61efa5ede192d3a99318042b50e7fb3d2fff2eb2
f-konashi <f-konashi@keyportsolutions.com>
2015-12-30 18:24:19 +0900

    javadocサンプル

---
+++ src/main/java/com/example/JavadocSampleApplication.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * エントリクラス.
+ * 
+ * @author fumihiro konashi
+ * @since 2015/12/07
+ * @version 1.0 {@Object}
+ * 
+ * @see Object {@link Object}を継承
+ * 
+ */
+@SpringBootApplication
+public class JavadocSampleApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(JavadocSampleApplication.class, args);
+  }
+}
